9ea882cd155c63aac408e6dca1c0cbcc16345e0a
Czakero <bartosz.pyrz96@gmail.com>
2018-07-01 20:29:14 +0200

    Contact implemented, js cleared

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-function changeBackground() {
-    if (document.body.style.backgroundImage == "url('background.jpg')" ) {
-        document.body.style.backgroundImage = "none";
-    } else {
-        document.body.style.backgroundImage = "url('background.jpg')";
-    }
-}
